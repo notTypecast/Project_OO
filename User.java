dfdad84ec77bf9daf77fb562c9741769eef0ae7d
notTypecast <ynkodizzy@gmail.com>
2020-05-01 22:42:47 +0300

    User class

--- User.java
+++ User.java
@@ -1,4 +1,23 @@
 
 public abstract class User {
+	
+	private String name;
+	private String mail;
+	
+	public String getName() {
+		return this.name;		
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	
+	public String getMail() {
+		return this.mail;
+	}
+	
+	public void setMail(String m) {
+		this.mail = m;
+	}
 
 }
