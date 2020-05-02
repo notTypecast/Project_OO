9ba85cf46f9d7a60c7d1f828b8d0432417b7b873
Frozo Web <frozoweb@gmail.com>
2020-05-02 19:06:20 +0300

    Merged erd branch to master

--- Notebook.java
+++ Notebook.java
@@ -1,4 +1,48 @@
 
-public class Notebook {
+public class Notebook extends Item{
+
+    
+    Notebook(String name,double price , String description , int stock , int id){
+
+        super(name , price , description , stock , id);     //calls Item constructor
+
+    }
+
+
+    //Variables
+
+    private int sections ;
+
+
+
+    //Methods
+
+//-----------------getDetails implementation-----------------
+
+    public String getDetails(){
+
+        return ", Sections are : " + Integer.toString(sections) ;
+
+    }
+
+
+//-------------------Setters-----------------------------------
+
+
+    public void setSections(int s){
+
+        this.sections = s ;
+
+    }
+
+
+//----------------Getters--------------------------------------
+
+    public int getSections(){
+
+        return this.sections ;
+
+    }
+
 
 }
--- Pen.java
+++ Pen.java
@@ -1,4 +1,63 @@
 
-public class Pen {
+public class Pen extends Item{
+
+
+	Pen(String name,double price , String description , int stock , int id){
+		
+		super(name,price , description , stock ,id);  	//calls Item constructor
+		
+	} 
+
+
+	//variables 
+
+    private String color ;
+	
+	private double tipSize ;
+
+
+	
+	//methods
+
+//----------------getDetails implementation---------
+	
+	
+	public String getDetails() {
+		
+		return ", Color of Pen is : " + this.color + ", Pen's tip is : " + Double.toString(tipSize) + " mm";
+				
+	}
+	
+	
+//---------------Setters-----------------------------
+	
+	
+	public void setColor(String c) {    	//color
+		
+		this.color = c ;
+		
+	}
+	
+	public void SetTipSize(double size) {		//TipSize
+		
+		this.tipSize = size ;
+		
+	}
+	
+
+//------------Getters-----------------------------
+	
+	
+	public String getColor() {		//Color
+		
+		return this.color ;
+		
+	}
+	
+	public double getTipSize() {		//TipSize
+		
+		return this.tipSize ;
+		
+	}
 
 }
